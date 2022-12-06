57f878cebce33baa5da005c10ed6c9baa3133cf7
hien nguyen <thuhiennguyen198@gmail.com>
2022-12-06 09:31:38 +0700

    add testing cases

---
+++ src/Testing.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Testing {
+    public static void main(String[] args) {
+//        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+//        for( int i = 0; i < cars.length; i++) {
+//            System.out.println(cars[i]);
+//        }
+//        System.out.println(cars.toString());
+//        for(int i = 0; i <= 10; i++){
+//            System.out.println(5 * i);
+//        }
+
+//        String names = "";
+//        String name;
+//        Scanner scanner = new Scanner(System.in);
+//        do{
+//            System.out.print("Enter a name: ");
+//            name = scanner.nextLine();
+//            names += " " + name;
+////            System.out.println(name.length());
+//        } while (name.length() < 5);
+//        System.out.println("The guest list is:" + names);
+    }
+}
