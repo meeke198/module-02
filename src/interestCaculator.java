ffa85ff359e8d14f92aef4faa12fb553baabf5c9
hien nguyen <thuhiennguyen198@gmail.com>
2022-12-06 11:02:00 +0700

    add caculate interest

---
+++ src/interestCaculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class interestCaculator {
+    public static void main(String[] args) {
+        double loan;
+        int month;
+        float interestRate;
+        double interest = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input the amount of money: ");
+        loan = input.nextDouble();
+                System.out.println("Input the month: ");
+        month = input.nextInt();
+        System.out.println("Input the interest rate: ");
+        interestRate = input.nextFloat();
+        for(int i = 1; i <= month; i++){
+            interest = loan * (interestRate/ 100)/12;
+            loan += interest;
+        }
+
+        System.out.println("Total interest is: " + Math.floor(loan));
+    }
+}
