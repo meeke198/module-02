1314aa1ecd7762240e4f1639743de16b17c31303
hien nguyen <thuhiennguyen198@gmail.com>
2022-12-06 09:57:05 +0700

    add isPrime

---
+++ src/isPrime.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class isPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao mot so: ");
+        int number = scanner.nextInt();
+        boolean check = true;
+        if(number < 2){
+            System.out.println(number + " is not a prime number");
+        } else {
+            for(int i = 2; i < number; i++){
+                if(number % i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+        }
+        if(check)
+            System.out.println(number + " is a prime number");
+
+        else
+            System.out.println(number + " is not a prime number");
+
+    }
+}
