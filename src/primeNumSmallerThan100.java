2b55af8bb041c2dc8443ddb251468ace749a4253
hien nguyen <thuhiennguyen198@gmail.com>
2022-12-06 14:48:52 +0700

    add prime number less than 100

---
+++ src/primeNumSmallerThan100.java
@@ -0,0 +1,20 @@
+public class primeNumSmallerThan100 {
+    public static void main(String[] args) {
+        boolean flag = true;
+        int num = 2;
+        while(num < 100){
+            flag = true;
+            for(int i = 2; i < num; i++){
+                if(num % i == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                System.out.println(num + " is prime numbers less than 100");
+//                System.out.println(num);
+            }
+            ++num;
+        }
+    }
+}
