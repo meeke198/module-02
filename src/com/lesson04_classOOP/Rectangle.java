12e7c4c7f8874554725682a8b223342433298799
hien nguyen <thuhiennguyen198@gmail.com>
2022-12-08 11:54:12 +0700

    add Rectangle

---
+++ src/com/lesson04_classOOP/Rectangle.java
@@ -0,0 +1,29 @@
+package com.lesson04_classOOP;
+
+import java.util.Scanner;
+
+
+
+
+public class Rectangle {
+    double height;
+    double width;
+    public Rectangle(){
+
+    }
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+    public String display(){
+        return "Rectangle {" + "width= " + width + ",height=" + height + "}";
+    }
+}
