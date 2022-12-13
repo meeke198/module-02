764d9c4140e8b6a5fe29d9e4bdf4744158b2d0a7
hien nguyen <thuhiennguyen198@gmail.com>
2022-12-13 09:59:56 +0700

    add rectangle

---
+++ src/com/lesson06Kethua/Rectangle.java
@@ -0,0 +1,66 @@
+package com.lesson06Kethua;
+
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double length = 1.0;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(double width, double length, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * this.length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + this.length);
+    }
+
+    @Override
+    public String toString() {
+        return "A Rectangle with width="
+                + getWidth()
+                + " and length="
+                + getLength()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+    }
+}
+
+
